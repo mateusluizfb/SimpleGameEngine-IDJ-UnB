b7a0cd900ffebfb2bcf421b4f664c61c9e81bc60
Mateus Luiz <mateuslfreitasb@gmail.com>
2025-08-28 20:44:21 -0300

    feat: Scaffold Vec2 and Rect

---
+++ include/Rect.h
@@ -0,0 +1,14 @@
+#ifndef REACT_H
+#define REACT_H
+
+class Rect {
+public:
+  float x, y, w, h;
+
+  Rect();
+  Rect(float x, float y, float w, float h);
+
+  // TODO: Implement the other operations
+};
+
+#endif
---
+++ src/Rect.cpp
@@ -0,0 +1,5 @@
+#include "Rect.h"
+
+Rect::Rect() : x(0.0), y(0.0), w(0.0), h(0.0) {}
+
+Rect::Rect(float x, float y, float w, float h) : x(x), y(y), w(w), h(h) {}
