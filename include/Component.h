5fcd2ace5a428629ce09098de1f128c0cfbf490a
Mateus Luiz <mateuslfreitasb@gmail.com>
2025-08-28 21:48:43 -0300

    feat: Scaffold Component and GameObject

---
+++ include/Component.h
@@ -0,0 +1,25 @@
+#ifndef COMPONENT_H
+#define COMPONENT_H
+
+/*
+Notes: 
+- virtual makes the method possible to override (polymorphism)
+- "= 0" makes it required for the method be implemented, and this class becomes abstract, so it cannot be instantiated 
+- `class GameObject` is needed in order to "forward declare" that his class exists, but it's going to me implemented later 
+*/
+
+class GameObject;
+
+class Component {
+protected:
+  GameObject& associated;
+
+public:
+  Component(GameObject &associated);
+  virtual ~Component();
+
+  virtual void Update(float dt) = 0;
+  virtual void Render() = 0;
+};
+
+#endif
---
+++ include/GameObject.h
@@ -0,0 +1,43 @@
+#ifndef GAME_OBJECT_H
+#define GAME_OBJECT_H
+
+#include "Component.h"
+#include "Rect.h"
+#include <vector>
+
+class GameObject {
+private:
+  bool isDead;
+  std::vector<Component *> components;
+
+public:
+  Rect box;
+
+  GameObject();
+  ~GameObject();
+
+  void Update(float dt);
+  void Render();
+  bool IsDead();
+  void RequestDelete();
+  void AddComponent(Component* cpt);
+  void RemoveComponent(Component* cpt);
+
+  template <typename T>
+  T GameObject::*GetComponent()
+  {
+    long unsigned int index;
+    for (index = 0; index < components.size(); index++)
+    {
+      // T *component = dynamic_cast<T *>(components[index].get());
+      T *component = dynamic_cast<T *>(components[index]);
+      if (component != nullptr)
+      {
+        return component;
+      }
+    }
+    return nullptr;
+  }
+};
+
+#endif
