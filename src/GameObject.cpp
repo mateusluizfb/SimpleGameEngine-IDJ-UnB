46475a6fd82673eece99d8caf400702478f68d5c
Mateus Luiz <mateuslfreitasb@gmail.com>
2025-08-30 15:11:05 -0300

    feat: Implement GameObject methods

--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,19 +1,43 @@
+#include "Log.h"
 #include "GameObject.h"
+
+#include <iostream>
 
 GameObject::GameObject() : isDead(false) {}
 
+/*
+Notes:
+- There might be cases where you need to **it (dereference twice), one for the iterator and another one for the pointer.
+*/
 GameObject::~GameObject() {
-  // TODO
+  Log::info("GAME_OBJECT - Destroying game object");
+
+  for (auto it = components.rbegin(); it != components.rend(); ++it)
+  {
+    delete *it;
+  }
+
+  components.clear();
 }
 
 void GameObject::Update(float dt)
 {
-  // TODO
+  Log::info("GAME_OBJECT - Updating game object");
+
+  for(Component *component : components)
+  {
+    component->Update(dt);
+  }
 }
 
 void GameObject::Render()
 {
-  // TODO
+  Log::info("GAME_OBJECT - Rendering game object");
+
+  for (Component *component : components)
+  {
+    component->Render();
+  }
 }
 
 bool GameObject::IsDead()
@@ -23,15 +47,21 @@ bool GameObject::IsDead()
 
 void GameObject::RequestDelete()
 {
-  // TODO
+  Log::info("GAME_OBJECT - Requesting delete");
+
+  isDead = true;
 }
 
 void GameObject::AddComponent(Component* component)
 {
-  // TODO
+  Log::info("GAME_OBJECT - Adding component to game object");
+
+  components.push_back(component);
 }
 
 void GameObject::RemoveComponent(Component *component)
 {
-  // TODO
+  Log::info("GAME_OBJECT - Removing component from game object");
+
+  components.erase(std::remove(components.begin(), components.end(), component), components.end());
 }
