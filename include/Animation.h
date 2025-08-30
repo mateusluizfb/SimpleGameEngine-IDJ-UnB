577970b61b5f24e64f150cc99afdbb6776ee5791
Mateus Luiz <mateuslfreitasb@gmail.com>
2025-08-30 16:55:04 -0300

    feat: Create animation object

---
+++ include/Animation.h
@@ -0,0 +1,13 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+class Animation {
+public:
+  int frameStart;
+  int frameEnd;
+  float frameTime;
+
+  Animation(int frameStart, int frameEnd, float frameTime) {}
+};
+
+#endif
