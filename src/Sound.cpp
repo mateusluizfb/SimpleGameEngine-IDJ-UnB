3fc9610b1cd0eac8f1540aeee4527b55b38375f5
Mateus Luiz <mateuslfreitasb@gmail.com>
2025-09-08 09:31:39 -0300

    feat: Implement Sound

---
+++ src/Sound.cpp
@@ -0,0 +1,43 @@
+#include "Sound.h"
+
+Sound::Sound() : chunk(nullptr) {}
+
+Sound::Sound(std::string file) : Sound() {
+    Open(file);
+}
+
+void Sound::Play(int times) {
+  if (chunk != nullptr)
+  {
+    channel = Mix_PlayChannel(-1, chunk, times - 1);
+  }
+}
+
+void Sound::Stop() {
+  if (chunk == nullptr)
+  {
+    throw std::runtime_error("SOUND - Can't stop a sound that is not open.");
+  }
+
+  Mix_HaltChannel(channel);
+}
+
+void Sound::Open(std::string file) {
+  if (chunk != nullptr)
+  {
+    throw std::runtime_error("SOUND - Can't open a sound that is already open.");
+  }
+
+  chunk = Mix_LoadWAV(file.c_str());
+  if (chunk == nullptr)
+  {
+    throw std::runtime_error("SOUND - Failed to load sound file: " + file + " - " + Mix_GetError());
+  }
+}
+
+Sound::~Sound() {
+  if (chunk != nullptr)
+  {
+    Mix_FreeChunk(chunk);
+  }
+}
