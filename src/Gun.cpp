03c37630b4c64fa47b1d59c6f3756d65264ccc3b
Mateus Luiz <mateuslfreitasb@gmail.com>
2025-10-10 09:17:43 -0300

    chore: Implement gun rotation and animation

--- src/Gun.cpp
+++ src/Gun.cpp
@@ -3,6 +3,7 @@
 #include "Animator.h"
 #include "Log.h"
 #include "Character.h"
+#include "InputManager.h"
 
 Gun::Gun(GameObject &associated, std::weak_ptr<GameObject> character)
   : Component(associated),
@@ -24,7 +25,12 @@ Gun::Gun(GameObject &associated, std::weak_ptr<GameObject> character)
 }
 
 void Gun::Shoot(Vec2 target) {
-  Log::debug("GUN - Shooting at target (" + std::to_string(target.x) + ", " + std::to_string(target.y) + ")");
+  shootSound.Play();
+
+  Vec2 direction = (target - associated.box.GetCenter()).Normalize();
+  angle = std::atan2(direction.y, direction.x);
+
+  cooldownState = 1;
 }
 
 std::weak_ptr<GameObject> Gun::GetCharacter() {
@@ -41,9 +47,52 @@ void Gun::Update(float dt)
 
   std::shared_ptr<GameObject> characterPtr = character.lock();
 
-  Vec2 offset = Vec2(30, 0);
-  Vec2 newCenter = characterPtr->box.GetCenter() + offset;
-  associated.box.SetCenter(newCenter.Rotate(angle));
+  // The angle rotates well, but the sprite is rendered on top of the 
+  // character's. so when it goes to the right of the character, we need to
+  // increase the offset, like an elipsis
+  Vec2 offset = Vec2(30, 0).Rotate(angle); 
+  Vec2 currentCenter = characterPtr->box.GetCenter();
+  Vec2 newCenter = (currentCenter + offset);
+
+  associated.box.SetCenter(newCenter);
+
+  Animator *animator = associated.GetComponent<Animator>();
+
+  cdTimer.Update(dt);
+
+  switch (cooldownState)
+  {
+    case 0:
+        break;
+    case 1:
+        if (cdTimer.Get() > 0.5) {
+          cooldownState = 2;
+          cdTimer.Restart();
+        }
+        break;
+    case 2:
+        if (cdTimer.Get() > 0.5) {
+          cooldownState = 3;
+          cdTimer.Restart();
+          reloadSound.Play();
+          animator->SetAnimation("reloading");
+        }
+        break;
+    case 3:
+        if (cdTimer.Get() > 0.5) {
+          animator->SetAnimation("idle");
+          cooldownState = 0;
+        }
+        break;
+  }
+
+  InputManager &inputManager = InputManager::GetInstance();
+
+  if (inputManager.MousePress(LEFT_MOUSE_BUTTON))
+  {
+    Vec2 target = Vec2(inputManager.GetMouseXWorld(), inputManager.GetMouseYWorld());
+    this->Shoot(target);
+  }
 }
 
 void Gun::Render() {
