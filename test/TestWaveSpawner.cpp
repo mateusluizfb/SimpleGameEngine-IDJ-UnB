feee78c4dd66d2814259be0ea2f50416a0a8adec
Mateus Luiz <mateus.freitas@piposaude.com.br>
2025-10-18 16:55:40 -0300

    chore: Update tests

--- test/TestWaveSpawner.cpp
+++ test/TestWaveSpawner.cpp
@@ -10,68 +10,45 @@
 #include "State.h"
 #include "SpriteRenderer.h"
 
-
-
-TEST(TestWaveSpawner, SpawnsZombieAfterCooldown) {
-	Game *game = &Game::GetInstance("Test Game", 800, 600);
-
-  Log::debug(" --- TestWaveSpawner Logs ----");
-
-
-	// Add a Character GameObject to the State
-	GameObject* characterGameObject = new GameObject();
-	Character* character = new Character(*characterGameObject, "assets/img/Player.png");
-	characterGameObject->AddComponent(character);
-	game->GetState().AddObject(characterGameObject);
-
-	GameObject go;
-	WaveSpawner spawner(go);
-	float dt = 6.0f; // cooldown is 5.0f in default wave
-	spawner.Update(dt);
-	SUCCEED();
-	delete game;
-}
-
-
-TEST(TestWaveSpawner, SpawnsCorrectZombieCount) {
-	Game *game = &Game::GetInstance("Test Game", 800, 600);
-
-  Log::debug(" --- TestWaveSpawner Logs ----");
-
-
-	// Add a Character GameObject to the State
-	GameObject* characterGameObject = new GameObject();
-	Character* character = new Character(*characterGameObject, "assets/img/Player.png");
-	characterGameObject->AddComponent(character);
-	game->GetState().AddObject(characterGameObject);
-
-	GameObject go;
-	WaveSpawner spawner(go);
-	for (int i = 0; i < 10; ++i) {
-		spawner.Update(6.0f);
-	}
-	SUCCEED();
-	delete game;
-}
-
-
 TEST(TestWaveSpawner, NextWaveTriggered) {
 	Game *game = &Game::GetInstance("Test Game", 800, 600);
+	Log::debug(" --- TestWaveSpawner Logs ----");
 
-  Log::debug(" --- TestWaveSpawner Logs ----");
+	State &state = game->GetState();
+	state.Start();
 
-
-	// Add a Character GameObject to the State
-	GameObject* characterGameObject = new GameObject();
-	Character* character = new Character(*characterGameObject, "assets/img/Player.png");
-	characterGameObject->AddComponent(character);
-	game->GetState().AddObject(characterGameObject);
-
-	GameObject go;
-	WaveSpawner spawner(go);
-	for (int i = 0; i < 5; ++i) {
-		spawner.Update(6.0f);
+	WaveSpawner *spawner = nullptr;
+	for (auto &obj : state.GetObjectArray()) {
+		spawner = obj->GetComponent<WaveSpawner>();
+		if (spawner) break;
 	}
-	SUCCEED();
+	ASSERT_NE(spawner, nullptr) << "No WaveSpawner found in object array";
+
+	// Initial object count
+	size_t initialCount = game->GetState().GetObjectArray().size();
+
+	// Run enough updates to trigger both zombie and npc spawns
+	for (int i = 0; i < 5; ++i) {
+		spawner->Update(6.0f);
+	}
+
+	size_t afterSpawnsCount = game->GetState().GetObjectArray().size();
+	EXPECT_GT(afterSpawnsCount, initialCount) << "objectArray should have more objects after spawns";
+
+	// Check if at least one NPC and one Zombie were spawned
+	bool foundNPC = false;
+	bool foundZombie = false;
+	auto objects = game->GetState().GetObjectArray();
+	for (auto &obj : objects) {
+		if (obj->GetComponent<Character>() && obj->GetComponent<Character>()->player == nullptr) {
+			foundNPC = true;
+		}
+		if (obj->GetComponent<Zombie>()) {
+			foundZombie = true;
+		}
+	}
+	EXPECT_TRUE(foundNPC) << "At least one NPC should have been spawned.";
+	EXPECT_TRUE(foundZombie) << "At least one Zombie should have been spawned.";
+
 	delete game;
 }
