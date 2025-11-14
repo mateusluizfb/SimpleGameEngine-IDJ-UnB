b3a37342ddc39ba385ac2ba0157f4c005a124385
Mateus Luiz <mateus.freitas@piposaude.com.br>
2025-11-14 12:38:54 -0300

    feat: Keep player inside the map bounds

--- include/PlayerController.h
+++ include/PlayerController.h
@@ -3,6 +3,11 @@
 
 #include "Component.h"
 
+#define X_UPPER_BOUND 1890.0f
+#define X_LOWER_BOUND 613.0f
+#define Y_UPPER_BOUND 2000.0f
+#define Y_LOWER_BOUND 479.0f
+
 class PlayerController : public Component {
 public:
   PlayerController(GameObject &associated);
